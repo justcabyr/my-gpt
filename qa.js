678b4d3d8829b78d55d8d732bfc0a834ea500d05
justcabyr <justcabyr@gmail.com>
2023-12-29 23:04:23 -0330

    created document qa

--- qa.js
+++ qa.js
@@ -6,3 +6,8 @@ import { YoutubeLoader } from 'langchain/document_loaders/web/youtube'
 import { CharacterTextSplitter } from 'langchain/text_splitter'
 import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
 
+const question = process.argv[2] || 'hi'
+const video = `https://youtu.be/zR_iuq2evXo?si=cG8rODgRgXOx9_Cn`
+
+export const createStore = (docs) =>
+  MemoryVectorStore.fromDocuments(docs, new OpenAIEmbeddings())
